d8a9a27bc78a488a25d7b508c8963be872066abf
HyoonJun <88166988+HyoonJun@users.noreply.github.com>
2021-09-20 07:52:30 +0000

    Add rune command

---
+++ commands/rune.js
@@ -0,0 +1,54 @@
+const { MessageEmbed } = require("discord.js");
+const { SlashCommandBuilder } = require("@discordjs/builders");
+const { formatHTML } = require("./utils/html-formatter.js");
+const runesReforged = require("../data/runesReforged.json");
+const { version } = require("../config.json");
+
+function findRuneName(name) {
+  // Traverse through layers of arrays/objects in JSON file.
+  for (let i = 0; i < runesReforged.length; i++) {
+    const tree = runesReforged[i];
+    const slots = tree.slots;
+
+    for (let j = 0; j < slots.length; j++) {
+      const runes = slots[j].runes;
+
+      for (let k = 0; k < runes.length; k++) {
+        if (runes[k].name.toLowerCase() === name.toLowerCase()) {
+          // Save the name of the rune tree so that it can be used later.
+          const rune = runes[k];
+          runes[k].tree = tree.name;
+          return runes[k];
+        }
+      }
+    }
+  }
+}
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName("rune")
+    .setDescription("Searches for a rune's description.")
+    .addStringOption((option) => {
+      return option
+        .setName("name")
+        .setDescription("Rune names cannot be abbreviated")
+        .setRequired(true)
+    }),
+  async execute(interaction) {
+    const runeName = interaction.options.getString("name");
+    const rune = findRuneName(runeName);
+
+    if (!rune) {
+      await interaction.editReply("That rune does not exist.");
+      return;
+    }
+
+    const embed = new MessageEmbed()
+      .setTitle(`${rune.name} (${rune.tree})`)
+      .setDescription(formatHTML(rune.longDesc))
+      .setFooter(`Patch ${version}`);
+
+    await interaction.editReply({ embeds: [embed] });
+  }
+};
