58b99ceb6cd1e99c45c81d414921ad9348ee0eca
HyoonJun <88166988+HyoonJun@users.noreply.github.com>
2021-09-20 08:10:32 +0000

    Add HTML formatting options
    * <b></b> and <i></i> tags now format the text
    
    * <li> now consists of a newline and a hyphen

--- commands/utils/html-formatter.js
+++ commands/utils/html-formatter.js
@@ -6,12 +6,16 @@ module.exports = {
   formatHTML(text) {
     /**
      * Skill descriptions may contain certain tags that we may want to
-     * replicate the behavior of, especially spacing. Thus, replace those tags
-     * with a newline.
+     * replicate the behavior of.
      */
     const str = text
       .replaceAll("<br>", "\n")
-      .replaceAll("<li>", "\n")
+      .replaceAll("<li>", "\n- ")
+      .replaceAll("<b>", "**")
+      .replaceAll("</b>", "**")
+      .replaceAll("<i>", "*")
+      .replaceAll("</i>", "*");
+
     return $("<div />").html(str).text();
   }
 };
